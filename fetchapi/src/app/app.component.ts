2d539e08cb9e4d2de6f5827f4b7d7054a1bd1949
peddiniranjanreddy <p.niranjan0@gmail.com>
2024-06-18 11:59:15 +0530

    Fetch API

---
+++ fetchapi/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
+  
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  posts:any;
+  
+  constructor(private service:PostService) {}
+  
+  ngOnInit() {
+      this.service.getPosts()
+        .subscribe(response => {
+          this.posts = response;
+        });
+  }
+}
